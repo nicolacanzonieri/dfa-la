07432f9e3c4d9d78837d29a32b46cfaad1d591bf
Nicola Canzonieri <113309267+nicolacanzonieri@users.noreply.github.com>
2024-04-28 10:57:01 +0000

    Added functions specifications

--- dfa-la.py
+++ dfa-la.py
@@ -1,6 +1,16 @@
-debug = False
+debug = False # Flag to enable/disable debug output
 
 def add_new_state(transition_table, input_counter, qx_0, qx_1):
+    """
+    Add transitions for a new state to the transition table.
+
+    Args:
+        transition_table (list): The transition table of the DFA.
+        input_counter (int): The index of the current state.
+        qx_0 (str): The destination state on input 0.
+        qx_1 (str): The destination state on input 1.
+
+    """
     if input_counter > 0:
         new_state_operations = [int(qx_0), int(qx_1)]
         transition_table.append([])
@@ -10,6 +20,13 @@ def add_new_state(transition_table, input_counter, qx_0, qx_1):
         transition_table[input_counter].extend(new_state_operations)
 
 def add_final_states(transition_table):
+    """
+    Add final states to the transition table.
+
+    Args:
+        transition_table (list): The transition table of the DFA.
+
+    """
     while True:
         final_state_code = str(input("\nFinal state (insert !end! to stop adding final states): q"))
         if final_state_code == "!end!":
@@ -18,6 +35,14 @@ def add_final_states(transition_table):
             transition_table[int(final_state_code)].append("x")
 
 def create_transition_table(transition_table, input_counter):
+    """
+    Guides the user to create the transition table for the DFA.
+
+    Args:
+        transition_table (list): The transition table of the DFA.
+        input_counter (int): The index of the current state.
+    
+    """
     while True:
         print("\n\nSTATE Q" + str(input_counter) + " - (Insert !end! to stop creating new states)\n")
 
@@ -37,6 +62,14 @@ def create_transition_table(transition_table, input_counter):
     if debug : print(transition_table)
 
 def run_dfa(transition_table, dfa_input):
+    """
+    Runs the DFA on the input string to determine acceptance.
+
+    Args:
+        transition_table (list): The transition table of the DFA.
+        dfa_input (str): The input string for the DFA.
+    
+    """
     current_state = 0
     start_char = 0
     end_char = 1
@@ -56,6 +89,13 @@ def run_dfa(transition_table, dfa_input):
         print("\nINPUT NOT ACCEPTED!")
 
 def take_input(transition_table):
+    """
+    Allows the user to input strings to test DFA acceptance.
+
+    Args:
+        transition_table (list): The transition table of the DFA.
+    
+    """
     while True:
         print("\n\n\nInsert an Input for the DFA - (insert !end! to stop the software):")
         dfa_input = str(input())
